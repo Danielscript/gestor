767d319f1a3873a76baa08cef4462f396e34cf7e
Danielscript <59482759+Danielscript@users.noreply.github.com>
2020-10-08 17:41:02 -0300

    Update todos_cursos.php

--- todos_cursos.php
+++ todos_cursos.php
@@ -15,6 +15,51 @@ echo '</prev>';
 		<link rel="stylesheet" href="bootstrap/css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	
+			<script>
+			//script de editar
+			function editar(id, txt_curso){
+				//criar um form de edição
+				let form = document.createElement('form')
+				form.action = 'curso_controller.php?acao=atualizar'
+				form.method = 'post'
+				form.className = 'row'
+				//criar um input para entrada do texto
+				let inputCurso = document.createElement('input')
+				inputCurso.type='text'
+				inputCurso.name='curso'
+				inputCurso.className = 'col-9 form-control'
+				inputCurso.value = txt_curso
+				//criar input um  hidden para guardar o id do curso
+				let inputId = document.createElement('input')
+				inputId.type = 'hidden'
+				inputId.name = 'id'
+				inputId.value = 'id'
+
+				//criar um button para envio do form
+				let button = document.createElement('button')
+				button.type = 'submit'
+				button.className = 'col-3 btn btn-info'
+				button.innerHTML = 'atualizar'
+
+				//inclui inputId no form
+				form.appendChild(inputId)
+				
+
+				//incluir inputcurso no form
+				form.appendChild(inputCurso)
+				//incluir buttom no form
+				form.appendChild(button)
+				//selecionar a div tarefa
+				let curso = document.getElementById('curso_'+id)
+				//limpar o texto da tarefa para inclusao do form
+
+				curso.innerHTML = ''
+				//incluir form na pagina
+				curso.insertBefore(form, curso[0])
+
+			}
+		</script>
 	</head>
 
 	<body>
@@ -46,10 +91,12 @@ echo '</prev>';
 										
 								<?php foreach($cursos as $indice => $curso) { ?>
 									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9"><?php $curso->curso ?> (<?php $curso->id_status ?>)</div>
+										<div class="col-sm-9"  id="curso_<?=  $curso->id ?>">
+											<?php echo $curso->curso ?> (<?php echo $curso->id_status ?>)
+										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
-											<i class="fas fa-edit fa-lg text-info"></i>
+											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?=  $curso->id ?>,'<?=  $curso->curso ?>')"></i>
 											<i class="fas fa-check-square fa-lg text-success"></i>
 										</div>
 									</div>
